754b758d9c3c82a22987bfae4c783469c857b5cb
Jack <jack.dillon266@gmail.com>
2021-02-04 22:57:46 +1100

    ternary operator for return

--- pipes/src/app/sort.pipe.ts
+++ pipes/src/app/sort.pipe.ts
@@ -6,10 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SortPipe implements PipeTransform {
 
   transform(array: any, order: string, propName: any): any {
-    if (order === 'asc') {
-      return array.sort((a, b) => (a[propName] < b[propName] ? -1 : 1));
-    } else if (order === 'desc') {
-      return array.sort((a, b) => (a[propName] > b[propName] ? -1 : 1));
-    }
+    return order === 'asc' ? array.sort((a, b) => (a[propName] < b[propName] ? -1 : 1))
+      : array.sort((a, b) => (a[propName] > b[propName] ? -1 : 1));
   }
 }
